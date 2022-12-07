c36ab186c92dfe1d87285648d31e4f5a6f88f5f9
linzx <356666293@qq.com>
2022-12-07 16:01:06 +0800

    update

--- quantumultX/task/testflight/Auto_join_TF.js
+++ quantumultX/task/testflight/Auto_join_TF.js
@@ -8,6 +8,9 @@ setTimeout(() => {
 }, 5000)
 
 !(async () => {
+  // loon 23LA2tmX
+  // stash elwvzipQ
+  // 微信键盘 iSTXkF4K
   ids = "23LA2tmX,elwvzipQ,iSTXkF4K";
   if (ids == "") {
     $notify("所有TF已加入完毕", "请手动关闭", "");
