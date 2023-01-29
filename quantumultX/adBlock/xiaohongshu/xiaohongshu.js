8a36f6584bf1902de1e9b2163d45c243c7d9af8b
linzx <356666293@qq.com>
2023-01-29 13:26:34 +0800

    update

--- quantumultX/adBlock/xiaohongshu/xiaohongshu.js
+++ quantumultX/adBlock/xiaohongshu/xiaohongshu.js
@@ -1,37 +1,49 @@
-/*
-version     v1.0.1
-updatetime  2022-11-04
-function    小红书去开屏广告、瀑布流广告、启动广告
-*/
+// 2023-01-28 21:40
 
-if (/^https?:\/\/edith\.xiaohongshu\.com\/api\/sns\/v\d\/system_service\/splash_config/.test($request.url)) {
-  var obj = JSON.parse($response.body);
-  obj.data.ads_groups.forEach((item) => {
-      item.start_time = "2208963661";
-      item.end_time = "2209050061";
-      item.ads.forEach((i) => {
-          i.start_time = "2208963661";
-          i.end_time = "2209050061";
+if (!$response.body) $done({});
+const url = $request.url;
+let obj = JSON.parse($response.body);
+
+if (obj.data) {
+  // 小红书-开屏广告-config
+  if (url.includes("/v1/system_service/config")) {
+    delete obj.data.store;
+    delete obj.data.splash;
+    delete obj.data.loading_img;
+  } else if (url.includes("/v2/system_service/splash_config")) {
+    // 小红书-开屏广告-splash_config
+    if (obj.data?.ads_groups) {
+      obj.data.ads_groups.forEach((i) => {
+        i.start_time = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
+        i.end_time = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
+        if (i.ads) {
+          i.ads.forEach((j) => {
+            j.start_time = 2208960000; // Unix 时间戳 2040-01-01 00:00:00
+            j.end_time = 2209046399; // Unix 时间戳 2040-01-01 23:59:59
+          });
+        }
       });
-  });
-  $done({
-      body: JSON.stringify(obj),
-  });
+    }
+  } else if (url.includes("/v6/homefeed")) {
+    // 小红书-信息流广告
+    let newItems = [];
+    for (let item of obj.data) {
+      // 去除直播
+      if (item.model_type === "live_v2") {
+        continue;
+        // 去除赞助,带货
+      } else if (item.ads_info) {
+        continue;
+        // 去除带货
+      } else if (item.card_icon) {
+        continue;
+      } else {
+        newItems.push(item);
+      }
+    }
+    obj.data = newItems;
+  }
 }
-if (/^https?:\/\/edith\.xiaohongshu\.com\/api\/sns\/v\d\/homefeed\?/.test($request.url)) {
-  var obj = JSON.parse($response.body);
-  obj.data = Object.values(obj.data).filter((item) => !item.is_ads);
-  $done({
-      body: JSON.stringify(obj),
-  });
-}
-if (/^https?:\/\/edith\.xiaohongshu\.com\/api\/sns\/v\d\/system_service\/config\?/.test($request.url)) {
-  var obj = JSON.parse($response.body);
-  //obj.data.tabbar.tabs = Object.values(obj.data.tabbar.tabs).filter((item) => !item.title == "购买");
-  delete obj.data.store;
-  delete obj.data.splash;
-  delete obj.data.loading_img;
-  $done({
-      body: JSON.stringify(obj),
-  });
-}
+
+body = JSON.stringify(obj);
+$done({ body });
