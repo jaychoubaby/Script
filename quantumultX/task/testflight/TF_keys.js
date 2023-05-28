2c83e0f650d8f728763bc833fd88a4d1536f3357
命格无双 <356666293@qq.com>
2023-05-28 09:03:30 +0800

    更新 TF_keys.js

--- quantumultX/task/testflight/TF_keys.js
+++ quantumultX/task/testflight/TF_keys.js
@@ -7,26 +7,48 @@ https://raw.githubusercontent.com/chouchoui/QuanX/master/Scripts/testflight/TF_k
 ^https:\/\/testflight\.apple\.com\/v3\/accounts/.*\/apps$ url script-request-header https://raw.githubusercontent.com/chouchoui/QuanX/master/Scripts/testflight/TF_keys.js
 [mitm]
 hostname = testflight.apple.com
-[task]
-10 * * * * * https://raw.githubusercontent.com/chouchoui/QuanX/master/Scripts/testflight/Auto_join_TF.js, tag=TestFlight自动加入, img-url= https://raw.githubusercontent.com/Orz-3/mini/master/Color/testflight.png, enabled=true
-
 *********************************/
 
-$prefs.setValueForKey(null, 'request_id')
-let url = $request.url
-let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
-const headers = Object.keys($request.headers).reduce((t, i) => ((t[i.toLowerCase()] = $request.headers[i]), t), {});
+const reg1 = /^https:\/\/testflight\.apple\.com\/v3\/accounts\/(.*)\/apps$/;
+const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/;
 
-let session_id = headers['x-session-id']
-let session_digest = headers['x-session-digest']
-let request_id = headers['x-request-id']
-$prefs.setValueForKey(key, 'key')
-$prefs.setValueForKey(session_id, 'session_id')
-$prefs.setValueForKey(session_digest, 'session_digest')
-$prefs.setValueForKey(request_id, 'request_id')
-if ($prefs.valueForKey('request_id') !== null) {
-    $notify('请关闭本脚本', '信息获取成功','')
-} else {
-    $notify('信息获取失败','请打开MITM H2开关并添加testflight.apple.com','')
+if (reg1.test($request.url)) {
+  $prefs.setValueForKey(null, "request_id");
+  let url = $request.url;
+  let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, "$2");
+  const headers = Object.keys($request.headers).reduce((t, i) => ((t[i.toLowerCase()] = $request.headers[i]), t), {});
+
+  let session_id = headers["x-session-id"];
+  let session_digest = headers["x-session-digest"];
+  let request_id = headers["x-request-id"];
+  $prefs.setValueForKey(key, "key");
+  $prefs.setValueForKey(session_id, "session_id");
+  $prefs.setValueForKey(session_digest, "session_digest");
+  $prefs.setValueForKey(request_id, "request_id");
+  if ($prefs.valueForKey("request_id") !== null) {
+    $notify("TestFlight自动加入", "信息获取成功", "");
+  } else {
+    $notify("TestFlight自动加入", "信息获取失败", "请添加testflight.apple.com");
+  }
+  $done({});
+} else if (reg2.test($request.url)) {
+  let appId = $prefs.valueForKey("APP_ID");
+  if (!appId) {
+    //https://testflight.apple.com/join/CcuVfzon
+    appId = "CcuVfzon";
+  }
+  let arr = appId.split(",");
+  const id = reg2.exec($request.url)[1];
+  arr.push(id);
+  arr = unique(arr).filter((a) => a);
+  if (arr.length > 0) {
+    appId = arr.join(",");
+  }
+  $prefs.setValueForKey(appId, "APP_ID");
+  $notify("TestFlight自动加入", `已添加APP_ID: ${id}`, `当前ID: ${appId}`);
+  $done({});
 }
-$done({})
+
+function unique(arr) {
+  return Array.from(new Set(arr));
+}
