98e567ef0fe0f2345d37e919aa171325ca1c2da3
back <356666293@qq.com>
2023-10-17 15:59:53 +0800

    update

---
+++ quantumultX/task/7A7/7A7.js
@@ -0,0 +1,56 @@
+const cookieName = '7A7'
+const back7A7BodyKey = 'back_7A7_body'
+const back = init()
+
+
+if ($request && $request.method != 'OPTIONS') {
+  const back7A7Body = JSON.stringify($response.body)
+  if (back7A7Body) back.setdata(back7A7Body, back7A7BodyKey)
+  back.log(`==>>> 行车数据：${back7A7Body}`)
+  back.msg(cookieName, `获取7A7数据: 成功`, ``)
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+back.done()
